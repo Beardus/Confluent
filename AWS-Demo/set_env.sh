f3c43cfca2a4ea765b154b8cf27e50adbc450e23
Beardus <46707916+Beardus@users.noreply.github.com>
2025-03-16 11:24:46 +0200

    Create set_env.sh

---
+++ AWS-Demo/set_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export TF_VAR_confluent_cloud_api_key=""
+export TF_VAR_confluent_cloud_api_secret=""
+export TF_VAR_s3_access_key_id=""
+export TF_VAR_s3_secret_key=""
+export TF_VAR_dynamo_access_key_id=""
+export TF_VAR_dynamo_secret_key=""
